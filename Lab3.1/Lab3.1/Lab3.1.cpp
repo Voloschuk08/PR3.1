efd5d1e7faa4ad5a15c13bda8494f7b9d3da89c3
Voloschuk08 <voloschuk07olia@gmail.com>
2024-09-18 13:52:39 +0300

    Add files via upload

---
+++ Lab3.1/Lab3.1/Lab3.1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; // вхідний параметр
+	double y; // результат обчислення виразу
+	double A; // проміжний результат - функціонально стала частина виразу
+	double B; // проміжний результат - функціонально змінна частина виразу
+
+	cout << "x = "; cin >> x;
+
+	y = 1 + 9 * x;
+	if (x <= 0)
+		y += log(fabs(sin(x))) + pow(x, 7);
+	if (x > 0 && x <= 3)
+		y += (cos(fabs(x + 1)) / sin(fabs(x + 1))) / 2.0;
+	if (x > 3)
+		y += 3 * x - pow(x, 5);
+
+	cout << endl;
+	cout << "1) y = " << y << endl;
+
+	y = 1 + 9 * x;
+	if (x <= 0)
+		y += log(fabs(sin(x))) + pow(x, 7);
+	else
+		if (x > 0 && x <= 3)
+			y += (cos(fabs(x + 1)) / sin(fabs(x + 1))) / 2.0;
+		else
+			y += 3 * x - pow(x, 5);
+
+	cout << "2) y = " << y << endl;
+	cin.get();
+	return 0;
+
+
+}
